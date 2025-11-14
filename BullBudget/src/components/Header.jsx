a71b742d4381460d274e62757758b9d5249f6dc1
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-14 17:50:35 -0500

    Updated logo

---
+++ BullBudget/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <>
+            <nav
+                className = {`${styles.container}`}
+            >
+
+            </nav>
+        </>
+    ) 
+}
+
+export default Header
