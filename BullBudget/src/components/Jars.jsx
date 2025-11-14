a25f7811f8c1132a88b10f2d4acac4ad7d1c104b
Huy <giahuychau07@gmail.com>
2025-11-14 17:12:14 -0500

    Jars components setup

---
+++ BullBudget/src/components/Jars.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Jars.css'
+
+
+const jarNum=6;
+
+function Jars() {
+  return (
+    <>
+        <h1>Your {jarNum} financial Jars</h1>
+        <div className="jars-container">
+            
+        </div>
+    </>
+  )
+}
+
+
+
+
+export default Jars
