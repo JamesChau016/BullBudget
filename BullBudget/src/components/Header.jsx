da506a118c5852071c4f054983bfe39604663a49
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-14 18:18:01 -0500

    Header update

--- BullBudget/src/components/Header.jsx
+++ BullBudget/src/components/Header.jsx
@@ -1,12 +1,29 @@
 import styles from './Header.module.css'
 
-const Header = () => {
+const Header = ({ children }) => {
     return (
         <>
             <nav
                 className = {`${styles.container}`}
             >
-
+                <div 
+                    className = {`${styles['header-app-info']}`}
+                >
+                    <img
+                        src = "public/USF_Logo.png"
+                        className = {`${styles['header-usf-logo']}`}
+                    ></img>
+                    <span
+                        className = {`${styles['header-app-name']}`} 
+                    >
+                        BullBudget
+                    </span>
+                </div>
+                <div
+                    className = {`${styles['header-navigation-container']}`}
+                >
+                    {children || 'Put your navigations here'}
+                </div>
             </nav>
         </>
     ) 
