333805bd14adeae668cb5903df250e5f70701fb4
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-14 17:50:35 -0500

    Updated logo

--- BullBudget/src/Landing/Landing.jsx
+++ BullBudget/src/Landing/Landing.jsx
@@ -1,4 +1,17 @@
 import styles from './Landing.module.css';
 import { useState } from 'react';
+import Header from '../components/Header'
 
+const Landing = () => {
+    return (
+        <>
+            <div
+                className = {`${styles.container}`}
+            >
+                <Header></Header>
+            </div>
+        </>
+    )
+}
 
+export default Landing
