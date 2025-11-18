fcbd698194c0fda3db72bb560ad143e77014b81a
Huy <giahuychau07@gmail.com>
2025-11-18 10:23:06 -0500

    Completed the jars (still missing the money information)

--- BullBudget/src/App.jsx
+++ BullBudget/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Toaster} from 'react-hot-toast'
 import './App.css'
 import Jars from './components/Jars.jsx'
 
 function App() {
   return (
-    <Jars />
+    <> 
+      <Jars />
+      <Toaster position="top-center" />
+    </>
+    
   )
 }
 
