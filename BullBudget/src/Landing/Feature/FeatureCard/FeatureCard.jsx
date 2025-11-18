07b9798b7725eb366500ac35813e92149f503e79
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-18 16:56:03 -0500

    Landing: Feature Section

---
+++ BullBudget/src/Landing/Feature/FeatureCard/FeatureCard.jsx
@@ -0,0 +1,34 @@
+import styles from './FeatureCard.module.css'
+
+const FeatureCard = ({ name, description, image, className, style }) => {
+    return (
+        <>
+            <div
+                className = {`${styles['card-container']} ${className}`}
+                style = {style}
+            > 
+                <div
+                    className = {`${styles['image-container']}`}
+                >
+                    {image === undefined ? 'image' : image}
+                </div>
+                <div
+                    className = {`${styles['card-content']}`}
+                >
+                    <h3
+                        className = {`${styles['feature-name']}`}
+                    >
+                        {name}
+                    </h3>
+                    <p
+                        className = {`${styles['feature-description']}`}
+                    >
+                        {description}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FeatureCard
