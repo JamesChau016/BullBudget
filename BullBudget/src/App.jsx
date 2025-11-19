1bd2ece5dda3b41893d7e75c939122af00d16fe4
Huy <giahuychau07@gmail.com>
2025-11-18 19:50:49 -0500

    login page (just the loook no function)

--- BullBudget/src/App.jsx
+++ BullBudget/src/App.jsx
@@ -1,18 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {Toaster} from 'react-hot-toast'
 import './App.css'
 import Jars from './components/Jars.jsx'
+import LoginPage from './components/Login.jsx'
 
 function App() {
-  return (
-    <> 
-      <Jars />
-      <Toaster position="top-center" />
-    </>
-    
-  )
+
+  const [loggedIn, setLoggedIn] = useState (false);
+
+
+  if (!loggedIn) {
+    return (
+      <>
+        <LoginPage loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
+        <Toaster position="top-center" />
+      </>
+      
+    )
+  }
+  else{
+    return (
+      <>
+        <Jars />
+        <Toaster position="top-center" />
+      </>
+    )
+  }
+
+  
 }
 
 export default App
