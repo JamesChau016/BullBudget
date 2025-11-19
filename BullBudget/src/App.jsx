f623989bb901211a2f758c6816979883476ab040
nnviet06 <nnviet2016@gmail.com>
2025-11-19 14:46:16 -0500

    Sync data from dashboard to Jars

--- BullBudget/src/App.jsx
+++ BullBudget/src/App.jsx
@@ -5,17 +5,24 @@ import {Toaster} from 'react-hot-toast'
 import './App.css'
 import Landing from './pages/Landing/Landing'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Jars from './pages/Jars/Jars'
 
 function App() {
+  // State chung. We can work on this later
+  const [budgets, setBudgets] = useState([
+    { id: 1, name: 'BullBucks', balance: 150 },
+    { id: 2, name: 'Dining Dollars', balance: 150 },
+    { id: 3, name: 'Tuition Fee', balance: 5000 },
+  ]);
+
   return (
     <>
-      <div
-        className = 'app-container'
-      >
-        <Dashboard></Dashboard>
+      <Toaster />
+      <div className='app-container'>
+        <Dashboard budgets={budgets} setBudgets={setBudgets} />
+        <Jars budgets={budgets} setBudgets={setBudgets} />
       </div>
     </>
-    
   )
 }
 
