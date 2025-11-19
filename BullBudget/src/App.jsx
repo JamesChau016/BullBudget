dbedffd97f028d955e897c62be8c6c46ca65d774
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-18 19:17:35 -0500

    HUGE MERGE

--- BullBudget/src/App.jsx
+++ BullBudget/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {Toaster} from 'react-hot-toast'
 import './App.css'
-<<<<<<< HEAD
 import Landing from './pages/Landing/Landing'
 import Dashboard from './pages/Dashboard/Dashboard'
 
@@ -15,15 +14,6 @@ function App() {
       >
         <Dashboard></Dashboard>
       </div>
-=======
-import Jars from './components/Jars.jsx'
-
-function App() {
-  return (
-    <> 
-      <Jars />
-      <Toaster position="top-center" />
->>>>>>> d53afd6c16330d8aa62093f63a0120fa38b61202
     </>
     
   )
