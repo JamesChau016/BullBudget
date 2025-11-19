7274361a0b4832ff48004944c6745a5d3b9b2afe
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-18 19:18:54 -0500

    hold on

--- BullBudget/src/App.jsx
+++ BullBudget/src/App.jsx
@@ -3,19 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {Toaster} from 'react-hot-toast'
 import './App.css'
-<<<<<<< HEAD
-import Landing from './pages/Landing/Landing'
-import Dashboard from './pages/Dashboard/Dashboard'
-
-function App() {
-  return (
-    <>
-      <div
-        className = 'app-container'
-      >
-        <Dashboard></Dashboard>
-      </div>
-=======
 import Jars from './components/Jars.jsx'
 
 function App() {
@@ -23,7 +10,6 @@ function App() {
     <> 
       <Jars />
       <Toaster position="top-center" />
->>>>>>> d53afd6c16330d8aa62093f63a0120fa38b61202
     </>
     
   )
