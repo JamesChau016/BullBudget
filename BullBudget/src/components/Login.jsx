dc5fb6c662cb8bb24ff327925cc41943361c6971
Huy <giahuychau07@gmail.com>
2025-11-18 19:50:49 -0500

    login page (just the loook no function)

--- BullBudget/src/components/Login.jsx
+++ BullBudget/src/components/Login.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import './Login.css'
+import toast from 'react-hot-toast';
 
-function LoginPage(){
+function LoginPage({setLoggedIn}){
+    const username= document.getElementById('username');
+    const password= document.getElementById('password');
+
+    const login = () => {
+        setLoggedIn(true);
+        toast.success('Logged in successfully!');   
+    }
+    
+
     return (
         <>
-            <h1>Welcome to BullBudget</h1>
-            <input type="text" placeholder="Username" /><br/>
-            <input type="password" placeholder="Password" /><br/>
-            <button>Login</button><br/>
-            <button>Sign up</button>
+            <h1 id='welcome-message'>Welcome to BullBudget</h1>
+            <div id='login-container'>
+                
+                <input id ='username' type="text" placeholder="Username" /><br/>
+                <input id ='password' type="password" placeholder="Password" /><br/>
+                <button id='login' onClick = {login}>Login</button><br/>
+                <button id='signup'>Sign up</button>
+            </div>
+            
         </>
     )
 }
