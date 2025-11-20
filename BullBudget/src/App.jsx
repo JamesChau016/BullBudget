3cb286c44ab38352b9dc1aac5ac41c6ef5de5092
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-19 20:19:26 -0500

    merged from Viet && add some small elements

--- BullBudget/src/App.jsx
+++ BullBudget/src/App.jsx
@@ -1,27 +1,65 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {Toaster} from 'react-hot-toast'
 import './App.css'
 import Landing from './pages/Landing/Landing'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Jars from './pages/Jars/Jars'
+import { initialBudgets } from './Data/budgetData'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+
+const NavigationWrapper = ({ budgets, setBudgets }) => {
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    navigate("/");
+  }, [])
+
+  return (
+    <Routes>
+      <Route 
+          path = "/" 
+          element = {
+            <Landing>  
+            </Landing>
+          }
+      ></Route>
+      <Route 
+        path = "/dashboard" 
+        element = {
+          <Dashboard
+            budgets = {budgets}
+            setBudgets = {setBudgets}
+          ></Dashboard>
+        }
+      ></Route>
+    </Routes>
+  )
+}
+
+const TestElement = () => {
+  const [budgets, setBudgets] = useState(initialBudgets);
+  return (
+      <>  
+        <Toaster />
+        <div className='app-container'>
+          <Dashboard budgets={budgets} setBudgets={setBudgets} />
+          <Jars budgets={budgets} setBudgets={setBudgets} />
+        </div>
+      </>
+  )
+}
 
 function App() {
   // State chung. We can work on this later
-  const [budgets, setBudgets] = useState([
-    { id: 1, name: 'BullBucks', balance: 150 },
-    { id: 2, name: 'Dining Dollars', balance: 150 },
-    { id: 3, name: 'Tuition Fee', balance: 5000 },
-  ]);
+  const [budgets, setBudgets] = useState(initialBudgets);
 
   return (
     <>
-      <Toaster />
-      <div className='app-container'>
-        <Dashboard budgets={budgets} setBudgets={setBudgets} />
-        <Jars budgets={budgets} setBudgets={setBudgets} />
-      </div>
+      <NavigationWrapper
+        budgets = {budgets}
+        setBudgets = {budgets}
+      >
+      </NavigationWrapper>
     </>
   )
 }
