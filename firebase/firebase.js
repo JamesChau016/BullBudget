daf0328521b006751a6958495dc0d64768e3014a
Huy <giahuychau07@gmail.com>
2025-11-20 23:53:07 -0500

    Make toaster work

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig)
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
