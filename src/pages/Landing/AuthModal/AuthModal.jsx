ce14cc928d1e84dca09aba5af36965f17394274b
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-20 23:02:36 -0500

    changing styles

--- src/pages/Landing/AuthModal/AuthModal.jsx
+++ src/pages/Landing/AuthModal/AuthModal.jsx
@@ -1,18 +1,40 @@
 import styles from './AuthModal.module.css' 
-import Login from '../../Login/Login'
+import Login from './Login/Login'
 import { useState } from 'react'
 
 const AuthModal = ({ display }) => {
     
     const [displayMode, setDisplayMode] = useState(display);
 
+    const renderContent = () => {
+        switch(displayMode) {
+            case 'login':
+                return (
+                    <>
+                        <Login> 
+                        </Login>
+                    </>
+                )
+        }
+    } 
+
+    const handleModalClick = () => {
+        setDisplayMode('none');
+    }
+
+    const displayNoneStyles = {
+        display: 'none',
+    }
+
+
     return (
         <>
             <div
                 className = {styles.container}
+                onClick = {handleModalClick}
+                style = {displayMode === 'none' ? displayNoneStyles : {}}
             >
-                <Login>
-                </Login>
+                {renderContent()}
             </div>
         </>
     )
