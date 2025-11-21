f3a1c95e405e8e2422340f9bb1b390dd6c9c9408
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-20 21:57:28 -0500

    changing Login mechanism

--- src/pages/Landing/AuthModal/AuthModal.jsx
+++ src/pages/Landing/AuthModal/AuthModal.jsx
@@ -1,9 +1,19 @@
 import styles from './AuthModal.module.css' 
+import Login from '../../Login/Login'
+import { useState } from 'react'
 
 const AuthModal = ({ display }) => {
+    
+    const [displayMode, setDisplayMode] = useState(display);
+
     return (
         <>
-            
+            <div
+                className = {styles.container}
+            >
+                <Login>
+                </Login>
+            </div>
         </>
     )
 }
