eff036b9e81f91dce4d39fe415809cf4786a4024
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-20 22:25:44 -0500

    Merged

--- src/pages/Dashboard/Welcome/Welcome.jsx
+++ src/pages/Dashboard/Welcome/Welcome.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from './Welcome.module.css'
 
 const Welcome = () => {
--- src/pages/Landing/Feature/Feature.jsx
+++ src/pages/Landing/Feature/Feature.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from './Feature.module.css'
 import FeatureCard from './FeatureCard/FeatureCard'
 
