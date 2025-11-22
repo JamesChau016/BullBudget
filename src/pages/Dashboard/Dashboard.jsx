7276d879049cbb6ea2eae924737d713510e59d46
Huy <giahuychau07@gmail.com>
2025-11-22 00:08:56 -0500

    signOut functionality

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,7 +6,10 @@ import Stats from './Stats/Stats';
 import BudgetList from '../../components/BudgetList/BudgetList';
 import Header from '../../components/Header/Header';
 import NavigationButton from '../../components/NavigationButton/NavigationButton.jsx';
-
+import { useNavigate } from 'react-router-dom';
+import { auth } from "../../firebase/firebase.js";
+import {signOut} from "firebase/auth";
+import toast from 'react-hot-toast';
 
 
 
@@ -14,8 +17,10 @@ import NavigationButton from '../../components/NavigationButton/NavigationButton
 const Dashboard = ({ budgets, setBudgets }) => {
   const navigate = useNavigate();
 
-  const handleLogoutClicked = () => {
+  const handleLogoutClicked = async () => {
+    await signOut(auth);
     navigate('/');
+    toast.success('Logout successful! See you next time.');
   }
   return (
     <>
